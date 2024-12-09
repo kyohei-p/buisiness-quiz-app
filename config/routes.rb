4bfa6d2803c156a72810aa12f460169813b72c0f
kyohei-p <ysdkyh11@gmail.com>
2024-12-10 06:35:20 +0900

    add:カテゴリーの一覧表示

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
       get "/auth/auth0/callback" => "auth0#create"
       get "/auth/failure" => "auth0#failure"
       get "/auth/logout" => "auth0#logout"
+
+      resources :categories
     end
   end
 end
---
+++ test/controllers/api/users/categories_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Api::Users::CategoriesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
