8534f46719f4511fb593b2b2630b3c96290cab76
kyohei-p <ysdkyh11@gmail.com>
2024-12-10 19:49:16 +0900

    fix:user_categoriesテーブルのカラム変更

--- db/migrate/20241209214348_create_user_categories.rb
+++ db/migrate/20241209214348_create_user_categories.rb
@@ -3,7 +3,7 @@ class CreateUserCategories < ActiveRecord::Migration[7.2]
     create_table :user_categories do |t|
       t.references :user, null: false, foreign_key: true
       t.references :category, null: false, foreign_key: true
-      t.string :is_selected
+      t.datetime :discarded_at
 
       t.timestamps
     end
